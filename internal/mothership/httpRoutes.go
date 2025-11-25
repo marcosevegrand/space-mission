9206c46410f9c41eeeaf00e0321ad2dc183c3b5b
franciscormartins <kiko.ribeiro.martins@gmail.com>
2025-11-25 09:16:10 +0000

    feat : mission painel html

--- internal/mothership/httpRoutes.go
+++ internal/mothership/httpRoutes.go
@@ -3,7 +3,11 @@ package mothership
 import "net/http"
 
 func RegisterHTTPRoutes(mux *http.ServeMux, m *Mothership) {
+
 	mux.Handle("/api/rovers", GetRoversHandler(m))
 	mux.Handle("/api/missions", GetMissionsHandler(m))
 	// Acrescenta outras rotas quando precisares
+
+	mux.Handle("/", http.FileServer(http.Dir("./groundcontrol")))
+
 }
