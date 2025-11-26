290e8d136e954054b303d51164a8556b396fc72c
yHugoSoares <hugoferreirasoares@gmail.com>
2025-11-26 20:05:16 +0000

    fix: fixed the problem about running it from inside cmd/mothership/

--- internal/mothership/httpHandlers.go
+++ internal/mothership/httpHandlers.go
@@ -1,10 +1,20 @@
 package mothership
 
 import (
+	"embed"
 	"encoding/json"
+	"io/fs"
 	"net/http"
-	"path/filepath"
 )
+
+// NOTE: You must move 'index.html', 'missions.html' and 'static/'
+// into a folder named 'frontend' inside 'internal/mothership/'.
+// Go embed does not support '../' paths.
+
+//go:embed frontend/*
+var frontendFS embed.FS
+
+var staticFS, _ = fs.Sub(frontendFS, "frontend/static")
 
 func (m *Mothership) HandleGetRovers(w http.ResponseWriter, r *http.Request) {
 	rovers := m.GetAllRovers()
@@ -26,15 +36,27 @@ func (m *Mothership) HandleGetMissions(w http.ResponseWriter, r *http.Request) {
 
 // Handler to serve the HTML UI at "/"
 func (m *Mothership) HandleIndex(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filepath.Join("cmd/groundcontrol", "index.html"))
+	data, err := frontendFS.ReadFile("frontend/index.html")
+	if err != nil {
+		http.Error(w, "index.html not found", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(data)
 }
 
 // Optionally, handler to serve static files (JS, CSS, etc.)
 func (m *Mothership) HandleStatic() http.Handler {
-	return http.StripPrefix("/static/", http.FileServer(http.Dir("cmd/groundcontrol/static")))
+	return http.StripPrefix("/static/", http.FileServer(http.FS(staticFS)))
 }
 
 // Handler to serve missions.html
 func (m *Mothership) HandleMissionsHTML(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filepath.Join("cmd/groundcontrol", "missions.html"))
+	data, err := frontendFS.ReadFile("frontend/missions.html")
+	if err != nil {
+		http.Error(w, "missions.html not found", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(data)
 }
